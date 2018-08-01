d0b2c1e22b2d6984522ca7e11bdb7179f03dadb6
ejrckd91 <ejrckd91@github.com>
2018-08-02 04:15:11 +0900

    dojo에 mysum함수사용해보기

--- mysite/dojo/urls.py
+++ mysite/dojo/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     #하나의함수(mysum)이 꼭 한가지 패턴에 매핑 될 필요 없음. 
     #디폴트인자를 넣어두면 1개,2개,3개일 때도 전부 매핑가능
-    url(r'^sum/(?P<x>\d+)/$', views.mysum),
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
+    url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
+
 ]
--- mysite/dojo/views.py
+++ mysite/dojo/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def mysum(request, x, y=0, z=0):#모든 view함수는 첫번쨰 인자가 request
+def mysum(request, numbers):#모든 view함수는 첫번쨰 인자가 request
     #request: HttpRequest
-    return HttpResponse(int(x) + int(y)+ int(z))#view의 인자로 넘겨 받은 값들은 모두 문자열
+    #numbers = "1/2/12/123/1234/123/121233"
+    result = sum(map(int,numbers.split("/")))
+    return HttpResponse(result)#view의 인자로 넘겨 받은 값들은 모두 문자열
